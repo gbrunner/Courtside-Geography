5f06b3c654896fe5e0b8882a280fc0f20b3507f4
Gregory Brunner <gbrunner@esri.com>
2016-08-15 20:57:47 -0500

    no message

--- RussellWestbrook/script.js
+++ RussellWestbrook/script.js
@@ -34,9 +34,9 @@ require([
   //  '//services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/KD_RegSeason_2015_16/FeatureServer/0';
   //'//services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/Scene_NBA_Test2_WFL/FeatureServer/0';
   var hexbinsFeatureServiceUrl =
-    '//services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/RW_RegSeason_2015_16/FeatureServer/1';  
+    'http://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/Thunder_Shots_Hexbins/FeatureServer/7';  
   var missesFeatureServiceUrl =
-    'http://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/RW_RegSeason_2015_16/FeatureServer/0';
+    'http://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/Thunder_Shots_Hexbins/FeatureServer/6';
     
   var tileLayer = new TileLayer({
     url: basketballCourtMapServiceUrl
